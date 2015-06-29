3168c36fc403ebe5a811bcac1fe7a2327153c48c
Rafael Miranda <rafmirlag@gmail.com>
2015-06-29 20:12:54 +0200

    Despliegue en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -6,8 +6,8 @@ exports.question = function(req, res) {
 // GET /quizes/answer
 exports.answer = function(req, res) {
   if (req.query.respuesta === 'Roma'){
-    res.render('quizes/answer', {respuesta: 'Correcto'});
+    res.render('quizes/answer', {respuesta: 'correcta'});
   }else{
-    res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    res.render('quizes/answer', {respuesta: 'incorrecta'});
   }
 };

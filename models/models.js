6792426402af69aa473f1ad45afa95a5f770ec88
Rafael Miranda <rafmirlag@gmail.com>
2015-07-02 19:19:31 +0200

    Integración de Postgres

--- models/models.js
+++ models/models.js
@@ -1,11 +1,31 @@
 var path = require( 'path' );
 
+// Postgres DATABASE_URL = postgres://czyakvrnwobvoe:enlEC3qID8RS7j7guwQrC35qG0@ec2-54-204-26-8.compute-1.amazonaws.com:5432/db7svnbsbgfogm
+
+// SQLite DATABASE_URL = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require( 'sequelize' );
 
-// Usar BBDD SQLite y creación de archivo quiz.sqlite:
+// Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize( null, null, null,
-    { dialect: "sqlite", storage: "quiz.sqlite" }
+    { dialect:  protocol,
+      protocol: protocol,
+      port:     port,
+      host:     host,
+      storage : storage, //solo SQLite (.env)
+      omitNull: true     // solo Postgres
+    }
   );
 
 // Importar la definición de la tabla Quiz en quiz.js

a09f68899fde9c12d1f16b0e93cbabf07e71b245
Rafael Miranda <rafmirlag@gmail.com>
2015-07-06 17:18:40 +0200

    Agregada funcionalidad de editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,8 +17,6 @@ exports.load = function(req, res, next, quizId) {
 // GET /quizes
 exports.index = function( req, res ){
 
-  //@FIX
-
   if (req.query.search){
     var search = req.query.search;
     words = search.replace(' ', '%'); // Se reemplazan los espacios por % para incluir todas las palabras en la búsqueda
@@ -61,7 +59,7 @@ exports.new = function(req, res){
   );
 
   res.render('quizes/new', {quiz: quiz, errors: []});
-}
+};
 
 // GET /quizes/create
 exports.create = function(req, res){
@@ -74,6 +72,35 @@ exports.create = function(req, res){
       res.render('quizes/new', {quiz: quiz, errors: err.errors});
     } else {
       quiz // guarda en DB los campos pregunta y respuesta de quiz
+      .save({fields: ["pregunta", "respuesta"]})
+      .then( function(){ res.redirect('/quizes')})  // Redirección HTTP (URL relativo) lista preguntas
+      }
+    }
+  );
+
+};
+
+// GET /quizes/:id/edit
+exports.edit = function (req, res) {
+  var quiz = req.quiz; // autoload de instancia quiz
+
+  // Usamos data para diferenciarlo del placeholder de otros formularios
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// GET /quizes/:id
+exports.update = function(req, res){
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  req.quiz
+  .validate()
+  .then(function(err){ // Importante: then funciona con validate con la versión 2.0.0 de sequelize
+    if(err){
+      res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+    } else {
+      req.quiz // guarda en DB los campos pregunta y respuesta de quiz
       .save({fields: ["pregunta", "respuesta"]})
       .then( function(){ res.redirect('/quizes')})  // Redirección HTTP (URL relativo) lista preguntas
       }

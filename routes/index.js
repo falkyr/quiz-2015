68e8e15458fe92e0093f7d004d3c2eab3ad1329c
Rafael Miranda <rafmirlag@gmail.com>
2015-07-20 17:57:00 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definicion de rutas de sesion
 router.get('/login', sessionController.new); // formulario login
@@ -35,6 +36,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 //Página del autor
 router.get('/author', quizController.author);
